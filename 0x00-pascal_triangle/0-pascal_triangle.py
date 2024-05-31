404e0bcf2fa0ca07625d359a2a088e8e30f62247
mohamed <mohamedhamdy994@gmail.com>
2024-05-31 00:24:35 -0700

    comit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""
-pascal triangle
+""" pascal triangle
 """
 
 
 def pascal_triangle(n):
+    """ returns pascal triangle
+    """
     if n <= 0:
         return []
-    # Initialize the Pascal's triangle with the first row
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]  # Start with the first element as 1
-        for j in range(1, i):
-            # Each interior element is the sum of the two elements above it
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)  # End with the last element as 1
-        triangle.append(row)
-    return triangle
+
+    pasTran = []
+
+    for i in range(n):
+        # first element
+        my_List = [1]
+        if i == 0:
+            pasTran.append(my_List)
+            continue
+
+        k = i - 1
+        for j in range(len(pasTran[k])):
+            if j + 1 == len(pasTran[k]):
+                # last element
+                my_List.append(1)
+                break
+            # Add two previous values to get current next value
+            nextVal = pasTran[k][j] + pasTran[k][j + 1]
+            my_List.append(nextVal)
+        pasTran.append(my_List)
+
+    return pasTran
