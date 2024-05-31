ff65b8b0bb3fd6af5f89b873d9d0c9e63e34cc59
mohamed <mohamedhamdy994@gmail.com>
2024-05-31 00:21:23 -0700

    commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""
+pascal triangle
+"""
+
 
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
     # Initialize the Pascal's triangle with the first row
     triangle = [[1]]
-    
     for i in range(1, n):
         row = [1]  # Start with the first element as 1
         for j in range(1, i):
@@ -14,5 +16,4 @@ def pascal_triangle(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)  # End with the last element as 1
         triangle.append(row)
-    
     return triangle
